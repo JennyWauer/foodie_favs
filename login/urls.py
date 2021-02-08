ace64d8f9163a1e46a6322ae8de648dc38978e79
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-08 09:17:50 -0600

    update style

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login),
+    path('', views.foodie_favs),
     path('register', views.register),
+    path('login', views.login),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def foodie_favs(request):
+    return render(request, 'foodie_favs.html')
+
 def login(request):
     return render(request, 'login.html')
 
