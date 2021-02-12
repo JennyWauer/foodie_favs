fcad5fab271900a8840b31899abb2080e06ed993
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-11 18:32:56 -0600

    add recipe model

--- foodie_app/models.py
+++ foodie_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 from login.models import User
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    incredients = models.ManyToManyField(Ingredient, related_name="recipies")
+    steps = models.ManyToManyField(Step, related_name="recipies")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
