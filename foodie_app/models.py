7bbe3f3e54da6c394ae85d3592d2047d2313833e
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-11 18:38:46 -0600

    add menu model

--- foodie_app/models.py
+++ foodie_app/models.py
@@ -6,6 +6,7 @@ class Recipe(models.Model):
     name = models.CharField(max_length=255)
     incredients = models.ManyToManyField(Ingredient, related_name="recipies")
     steps = models.ManyToManyField(Step, related_name="recipies")
+    menus = models.ManyToManyField(Menu, related_name="menus")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,5 +17,11 @@ class Ingredient(models.Model):
 
 class Step(models.Model):
     step = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Menu(models.Model):
+    week_start = models.DateField()
+    week_end = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
