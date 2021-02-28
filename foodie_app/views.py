8da3534f4e7ababa13620c5189e2895c2c4f727e
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-27 20:13:37 -0600

    update add ingredient view

--- foodie_app/views.py
+++ foodie_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
 
@@ -6,7 +7,7 @@ from login.models import User
 
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def home(request):
     if 'userid' in request.session:
@@ -37,27 +38,30 @@ def user_profile(request, user_id):
         return render(request, 'user_profile.html', context)
     return redirect('/login')
 
+@csrf_exempt
 def add_recipe(request):
     if request.method == "POST":
         ingredients = request.POST.get('ingredients')
         ingredients = json.loads(ingredients)
         steps = request.POST.get('steps')
         steps = json.loads(steps)
-        recipe_name = request.POST.get('name')
+        name = request.POST.get('name')
         recipe_name = json.loads(name)
         user_for_recipe = request.POST.get('user')
 
-        recipe_model, Recipe.objects.get_or_create(name=recipe_name['name'], user=user_for_recipe[user])
+        recipe_model, created = Recipe.objects.get_or_create(name=recipe_name['name'], user=user_for_recipe['user'])
 
-        for ingredient in ingredients:
-            ingredient_model, created = Ingredient.objects.get_or_create(ingredient=ingredient['ingredient'], amount=ingredient['amount'])
-            # new_recipe.ingredients.add(ingredient_model)
-            return JsonResponse({'status':200, 'created': created})
+        # for ingredient in ingredients:
+        #     ingredient_model, created = Ingredient.objects.get_or_create(ingredient=ingredient['ingredient'], amount=ingredient['amount'])
+        #     ingredient_model.save()
+        #     # new_recipe.ingredients.add(ingredient_model)
+        #     return JsonResponse({'status':200, 'created': created})
 
-        for step in steps:
-            step_model, created = Step.objects.get_or_create(step=step['step'])
-            # new_recipe.step.add(step_model)
-            return JsonResponse({'status':200, 'created': created})
+        # for step in steps:
+        #     step_model, created = Step.objects.get_or_create(step=step['step'])
+        #     step_model.save()
+        #     # new_recipe.step.add(step_model)
+        #     return JsonResponse({'status':200, 'created': created})
 
 def add_item(request):
     if request.method == 'POST':
@@ -68,17 +72,15 @@ def log_off(request):
     request.session.clear()
     return redirect('/')
 
+@csrf_exempt
 def add_ingredient(request):
     print("successful request")
     if request.method == "POST":
         quantity = request.POST.get('quantity')
-        print(quantity)
-        quantity = json.loads(quantity)
+        print('Quantity: ' + quantity)
         measurement = request.POST.get('measurement')
-        measurement = json.loads(measurement)
         name = request.POST.get('name')
-        name = json.loads(ingredients)
-        ingredient_model, created = Ingredient.objects.get_or_create(quantity=quantity['quantity'], measurement=measurement['measurement'], name=name['name'])
+        ingredient_model, created = Ingredient.objects.get_or_create(quantity=quantity, measurement=measurement, name=name)
         ingredient_model.save()
         return HttpResponse("It worked!")
 
