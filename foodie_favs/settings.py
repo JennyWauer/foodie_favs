41cc9ba7afc94bc6bf32b631cd5c95ddd2d3948c
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-03-17 15:22:18 -0500

    edit s3 bucket settings

--- foodie_favs/settings.py
+++ foodie_favs/settings.py
@@ -121,4 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = config(f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'foodie_favs/static'),
+]
+STATIC_URL = config(f'https://{AWS_S3_CUSTOM_DOMAIN}/static/')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
